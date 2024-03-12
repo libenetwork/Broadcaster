c8f0d3c980393d0d197c6507d29ce7ee342e9224
libnetwork <mylittleponyg5@gmail.com>
2024-03-12 20:24:08 +0100

    git remote add origin https://github.com/libenetwork/Broadcasterapp.git

--- www/js/coockie.js
+++ www/js/coockie.js
@@ -19,17 +19,7 @@ function getCookie(cname) {
     }
     return "";
 }
-String.prototype.hashCode = function() {
-    var hash = 0,
-        i, chr;
-    if (this.length === 0) return hash;
-    for (i = 0; i < this.length; i++) {
-        chr = this.charCodeAt(i);
-        hash = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-}
+
 
 function checkCookie() {
     let username = getCookie("username");
