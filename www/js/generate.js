c4750d405361addbfaa3460480e45486ddf8c179
libnetwork <mylittleponyg5@gmail.com>
2024-03-14 11:56:58 +0100

    git remote add origin https://github.com/libenetwork/Broadcasterapp.git

--- www/js/generate.js
+++ www/js/generate.js
@@ -1,7 +1,7 @@
 import { client } from "https://cdn.jsdelivr.net/npm/@gradio/client@0.1.4/dist/index.min.js";
 
 webhookclient.addEventListener("message", e => {
-    let data = JSON.parse(decodeURI(e.data)).name.replaceAll("+", " ") + " задон+атив " + JSON.parse(decodeURI(e.data)).amount + " гривень і сказав «" + JSON.parse(decodeURI(e.data)).message.replaceAll("+", " ") + "»";
+    let data = JSON.parse(decodeURI(e.data)).name.replaceAll("+", " ") + " задон+атив " + JSON.parse(decodeURI(e.data)).amount + " гривень і сказав «" + JSON.parse(decodeURI(e.data)).message.replaceAll("+", " ").replaceAll("%2C", ",").replaceAll("%3B", ";").replaceAll("%3A", ":") + "»";
     generate(data, e)
 })
 async function generate(text, e){
