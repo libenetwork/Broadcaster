5e820d0637ec075c289a09acef1e4e5fb8ae5fbf
libnetwork <mylittleponyg5@gmail.com>
2024-03-14 19:35:32 +0100

    git remote add origin https://github.com/libenetwork/Broadcasterapp.git

--- www/js/generate.js
+++ www/js/generate.js
@@ -1,11 +1,12 @@
 import { client } from "https://cdn.jsdelivr.net/npm/@gradio/client@0.1.4/dist/index.min.js";
 
 webhookclient.addEventListener("message", e => {
-    let data = JSON.parse(decodeURI(e.data)).name.replaceAll("+", " ") + " задон+атив " + JSON.parse(decodeURI(e.data)).amount + " гривень і сказав «" + JSON.parse(decodeURI(e.data)).message.replaceAll("+", " ").replaceAll("%2C", ",").replaceAll("%3B", ";                     ").replaceAll("%3A", ":") + "»";
+    let data = JSON.parse(decodeURI(e.data)).name.replaceAll("+", " ").replaceAll("%2C", ",").replaceAll("%3B", ";").replaceAll("%3A", ":") + " задон+атив " + JSON.parse(decodeURI(e.data)).amount + " гривень і сказав «" + JSON.parse(decodeURI(e.data)).message.replaceAll("+", " ").replaceAll("%2C", ",").replaceAll("%3B", ";").replaceAll("%3A", ":") + "»";
 
     generate(data, e)
 })
 async function generate(text, e){
+    console.log("generation started!");
     const app = await client("https://robinhad-ukrainian-tts.hf.space/");
     const result = await app.predict("/predict", [
         text, // string  in 'Input' Textbox component
