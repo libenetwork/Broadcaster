ed777ab150dc49fea834f960735ff3a8faacdb15
libnetwork <mylittleponyg5@gmail.com>
2024-03-17 16:53:54 +0100

    git remote add origin https://github.com/libenetwork/Broadcasterapp.git

--- www/js/sources.js
+++ www/js/sources.js
@@ -9,7 +9,7 @@ let sources = [];
 let audiostreams = [];
 
 function add_source(){
-    if (document.getElementsByClassName("microphone")[0].innerText != "Додати пристрій"){
+    if (document.getElementsByClassName("microphone")[0].innerText !== "Додати пристрій"){
         if (sources.find((e) => e.name === document.getElementsByClassName("microphone")[0].innerText) === undefined) {
             let append = "<td>" + document.getElementsByClassName("microphone")[0].innerText + "</td><td><input type=\"range\" id=\"volume" + (sources.length) + "\" name=\"volume\" min=\"0\" max=\"100\" value='100' /></td><td><div class=\"bar\"> <div class=\"indicator\" id=\"indicator"+ (sources.length) +"\"></div>\</div></td>";
             // document.getElementsByClassName("sources")[0].innerHTML += append;
@@ -39,42 +39,86 @@ function remove_source(){
 }
 function create_stream(id){
     // sources[id].device.getUserMedia({});
-    const constraints = {deviceId : {exact: sources[id].device.deviceId}};
-    navigator.mediaDevices.getUserMedia({
-        audio: constraints
-    }).then((stream) => {
-        const context = new AudioContext();
-        const source = context.createMediaStreamSource(stream);
-        const analyzer = context.createAnalyser();
-        const destination = context.createMediaStreamDestination();
-        const gain = context.createGain();
-        source.connect(gain);
-        gain.connect(destination);
-        // destination.connect(analyzer);
-        document.getElementById('volume' + id).onchange = function() {
-            gain.gain.value = Number(this.value)/100; // Any number between 0 and 1.
-        };
-        gain.gain.value = Number(document.getElementById('volume' + id).value)/100;
-        gain.connect(analyzer);
 
-        //source.connect(analyzer);
+    if (sources[id].device === undefined){
+        const audio = true;
+        navigator.mediaDevices.getDisplayMedia({
+            video:  true ,
+            audio: audio
+        })
+    .then((videostream, audiostream) => {
+            const context = new AudioContext();
+            const source = context.createMediaStreamSource(audiostream);
+            const analyzer = context.createAnalyser();
+            const destination = context.createMediaStreamDestination();
+            const gain = context.createGain();
+            source.connect(gain);
+            gain.connect(destination);
+            // destination.connect(analyzer);
+            document.getElementById('volume' + id).onchange = function () {
+                gain.gain.value = Number(this.value) / 100; // Any number between 0 and 1.
+            };
+            gain.gain.value = Number(document.getElementById('volume' + id).value) / 100;
+            gain.connect(analyzer);
 
-        // The array we will put sound wave data in
-        const array = new Uint8Array(analyzer.fftSize);
+            //source.connect(analyzer);
 
-        function getPeakLevel() {
-            analyzer.getByteTimeDomainData(array);
-            return array.reduce((max, current) => Math.max(max, Math.abs(current - 127)), 0) / 128;
-        }
+            // The array we will put sound wave data in
+            const array = new Uint8Array(analyzer.fftSize);
 
-        function tick() {
-            const peak = getPeakLevel();
-            document.getElementById('indicator' + id).style.width = `${peak * 100}%`;
-            requestAnimationFrame(tick);
-        }
-        tick();
-        audiostreams[audiostreams.length] = destination.stream;
-    });
+            function getPeakLevel() {
+                analyzer.getByteTimeDomainData(array);
+                return array.reduce((max, current) => Math.max(max, Math.abs(current - 127)), 0) / 128;
+            }
+
+            function tick() {
+                const peak = getPeakLevel();
+                document.getElementById('indicator' + id).style.width = `${peak * 100}%`;
+                requestAnimationFrame(tick);
+            }
+
+            tick();
+            audiostreams[audiostreams.length] = destination.stream;
+        });
+    }else {
+        const constraints = {deviceId: {exact: sources[id].device.deviceId}};
+        navigator.mediaDevices.getUserMedia({
+            audio: constraints
+        }).then((stream) => {
+            const context = new AudioContext();
+            const source = context.createMediaStreamSource(stream);
+            const analyzer = context.createAnalyser();
+            const destination = context.createMediaStreamDestination();
+            const gain = context.createGain();
+            source.connect(gain);
+            gain.connect(destination);
+            // destination.connect(analyzer);
+            document.getElementById('volume' + id).onchange = function () {
+                gain.gain.value = Number(this.value) / 100; // Any number between 0 and 1.
+            };
+            gain.gain.value = Number(document.getElementById('volume' + id).value) / 100;
+            gain.connect(analyzer);
+
+            //source.connect(analyzer);
+
+            // The array we will put sound wave data in
+            const array = new Uint8Array(analyzer.fftSize);
+
+            function getPeakLevel() {
+                analyzer.getByteTimeDomainData(array);
+                return array.reduce((max, current) => Math.max(max, Math.abs(current - 127)), 0) / 128;
+            }
+
+            function tick() {
+                const peak = getPeakLevel();
+                document.getElementById('indicator' + id).style.width = `${peak * 100}%`;
+                requestAnimationFrame(tick);
+            }
+
+            tick();
+            audiostreams[audiostreams.length] = destination.stream;
+        });
+    }
 }
 function remove_stream(id){
     try {
