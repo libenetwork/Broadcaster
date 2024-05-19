7e8c4a329d40468c588140620670180e83aa5bf9
libenetwork <mylittleponyg5@gmail.com>
2024-05-19 17:24:58 +0300

    youtube redirect

--- broadcaster-web/js/youtube_auth.js
+++ broadcaster-web/js/youtube_auth.js
@@ -17,15 +17,16 @@ function youtube_authorise() {
         '/youtube/auth'
     );
     youtubesocket.addEventListener("message", e => {
-        //Youtube_url = e.data;
+              //Youtube_url = e.data;
        // console.log("yes");
-        if (e.data.toString().startsWith("http")) {
-            ipc.send("window", e.data);
-            ipc.on('response', function (e, response) {
-             //   console.log(response);
-                youtubesocket.send(response);
+       if (e.data.toString().startsWith("http")) {
+        //  ipc.send("window", e.data);
+      /*    ipc.on('response', function (e, response) {
+           //   console.log(response);
+              youtubesocket.send(response);*/
+              location.href = e.data;
 
-            });
+          }});
         }else if (e.data.toString().startsWith("{\"tokens\":")){
             console.log(e.data);
             localStorage.setItem("youtube_token", e.data);
